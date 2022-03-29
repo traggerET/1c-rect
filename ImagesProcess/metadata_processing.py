da7953b22c7f2d38e03b83377abc8f661b385781
traggerET <ttixonin@mail.ru>
2022-03-29 19:59:03 +0300

    almost done

--- ImagesProcess/metadata_processing.py
+++ ImagesProcess/metadata_processing.py
@@ -5,16 +5,22 @@ def orient_position(description):
                     ]
     rows = description["rows_count"]
     row_len = description["rows_len_count"]
+    current = description["position"]
+
+    # just splitted matrix into a grid
     x_grid = [x for x in range(rows // 3, rows + 1, rows // 3)]
     y_grid = [y for y in range(row_len // 3, row_len + 1, row_len // 3)]
     x_left = y_left = 0
-    current = description["position"]
+
     for i, x_right in enumerate(x_grid):
         for j, y_right in enumerate(y_grid):
+
+            # check if left-upper corner fits into grid dividers
             if x_left <= current[1] <= x_right \
                     and y_left <= current[0] <= y_right:
                 description["oriented_pos"] = orientations[i + j]
                 break
+
             y_left = y_right
         x_left = x_right
 
